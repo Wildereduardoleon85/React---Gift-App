52586f919a619ebe4f20f67acdc7740394679831
Wilder <wilder_eduardo85@hotmail.com>
2020-09-24 18:34:13 -0300

    docs agregados

---
+++ docs/precache-manifest.7b72421d9e381fb9f1150445f5d3f8c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ffdf8637eebffdfb35fe066d696f42a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d4f1aacdbcc72d0f42",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "5ebe41a1e8551bcde621",
+    "url": "/static/js/2.a930a444.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a930a444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d4f1aacdbcc72d0f42",
+    "url": "/static/js/main.e5300678.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
